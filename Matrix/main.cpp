869b27336e730a8674e8d312f1f709e0c4839f11
user <user@WIN-S3FLAGBG853>
2024-06-15 00:36:07 +0300

    Matrix

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,41 +1,48 @@
-// динамическое выделение памяти, очистку памяти,
-//заполнение матрицы с клавиатуры, заполнение случайными значениями, отображение матрицы,
+//заполнение матрицы с клавиатуры????
 //арифметические операции(+, –, *), поиск максимального и минимального элемента.*/
 
 #include <iostream>
 #include <iomanip>
-//#include <string.h>
-//#include <typeinfo>
 
 using namespace std;
 
-template <class T>
 class Matrix
 {
-public:
 	int str = 0;
 	int col = 0;
-	T** mass = nullptr;
+	int** mass = nullptr;
+public:
 	Matrix()
 	{
 		cout << "\nЗапустился конcтруктор по умолчанию " << this << "\n";
-
-		this->str = 3;
-		this->col = 3;
-		this->mass = new T * [str];
-		for (int i = 0; i < str; i++) {
-			mass[i] = new T[col]{};
-		}
-	};
+		this->str = 0;
+		this->col = 0;
+		this->mass = nullptr;
+	}
 	Matrix(int str, int col) {
 		cout << "\nЗапустился конcтруктор с параметрами " << this << "\n";
 		this->str = str;
 		this->col = col;
-		this->mass = new T * [str];
-		for (int i = 0; i < str; i++) {
-			mass[i] = new T[col]{};
+		this->mass = new int* [str];
+		for (int i = 0; i < str; i++)
+		{
+			mass[i] = new int[col] {};
 		}
-	};
+	}
+	Matrix(const Matrix& obg)
+	{
+		cout << "\nЗапустился конcтруктор копирования " << this << "\n";
+		this->str = obg.str;
+		this->col = obg.col;
+		this->mass = new int* [str];
+		for (int i = 0; i < str; i++) {
+			mass[i] = new int[col] {};
+			for (int j = 0; j < col; j++)
+			{
+				mass[i][j] = obg.mass[i][j];
+			}
+		}
+	}
 	~Matrix() {
 		cout << "\nЗапустился деструктор " << this << "\n";
 		for (int i = 0; i < str; i++) {
@@ -43,12 +50,70 @@ public:
 		}
 		delete[]mass;
 	};
+
+	// Геттеры и сеттеры
+	int getStr() const { return str; }
+	int getCol() const { return col; }
+	int getMass(int str, int col) const { return mass[str][col]; }
+	void setMass(int str, int col, int data) { mass[str][col] = data; }
+
+	// Операторы
+	Matrix& operator =(const Matrix& obg)
+	{
+		if (this == &obg) return *this;//проверка на самого себя
+		if (this->str != obg.str) //Проверка на равенство строк
+		{
+			// удаляем память
+			for (int i = 0; i < this->str; i++) { delete[]mass[i]; }
+			delete[]mass;
+			// выделяем новую память
+			this->str = obg.str;
+			this->col = obg.col;
+			this->mass = new int* [this->str];
+			for (int i = 0; i < this->str; i++) {
+				mass[i] = new int[this->col] {};
+				for (int j = 0; j < col; j++)
+				{
+					mass[i][j] = obg.mass[i][j];
+				}
+			}
+			return *this;
+		}
+		else
+		{
+			if (this->str != obg.str) //Проверка на равенство столбцы
+			{
+				// удаляем память (часть)
+				for (int i = 0; i < this->str; i++) { delete[]mass[i]; }
+				// выделяем новую память
+				this->col = obg.col;
+				for (int i = 0; i < this->str; i++) {
+					mass[i] = new int[this->col] {};
+					for (int j = 0; j < col; j++)
+					{
+						mass[i][j] = obg.mass[i][j];
+					}
+				}
+			}
+			return *this;
+		}
+		for (int i = 0; i < str; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				mass[i][j] = obg.mass[i][j];
+			}
+		}
+		return *this;
+	}
+
+	// Методы
 	// Вывод на экран
 	void printMatrix()
 	{
 		for (int i = 0; i < str; i++) {
 			for (int j = 0; j < col; j++) {
-				cout << setw(4) << this->mass[i][j];
+				cout << setw(6) << this->mass[i][j]; //6 мест на вывод числа
 			}
 			cout << endl;
 		}
@@ -56,17 +121,19 @@ public:
 	}
 	// Заполнение случайными числами
 	void inicialis() {
+		cout << "\nЗаполнение случайными числами " << this << "\n";
+
 		for (int i = 0; i < this->str; i++)
 		{
 			for (int j = 0; j < this->col; j++)
 			{
-				mass[i][j] = rand() % 10;
+				mass[i][j] = rand() % 100;
 			}
 		}
 	};
-	T max()
+	int max()
 	{
-		T max = mass[0][0];
+		int max = mass[0][0];
 		for (int i = 0; i < str; i++)
 		{
 			for (int j = 0; j < col; j++)
@@ -79,9 +146,9 @@ public:
 		}
 		return max;
 	}
-	T min()
+	int min()
 	{
-		T min = mass[0][0];
+		int min = mass[0][0];
 		for (int i = 0; i < str; i++)
 		{
 			for (int j = 0; j < col; j++)
@@ -96,113 +163,183 @@ public:
 	}
 
 	//Консруктор с оператором
+	//Matrix(Matrix& mat1, Matrix& mat2, char oper) {
+	//	cout << "\nЗапустился конcтруктор с оператором " << this << "\n";
+	//	if (typeid(mat1.mass).name() != typeid(mat2.mass).name())
+	//	{
+	//		cout << "\nТипы матриц не совпадают \n";
+	//	}
+	//	else
+	//		switch (oper) {
+	//		case '+':
+	//		{
+	//			if (mat1.str != mat2.str && mat1.col != mat2.col)
+	//			{
+	//				cout << "\nТипы матриц не совпадают \n";
+	//			}
+	//			else
+	//			{
+	//				this->str = mat1.str;
+	//				this->col = mat1.col;
+	//				this->mass = new int* [str];
+	//				for (int i = 0; i < str; i++) {
+	//					mass[i] = new int[col] {};
+	//					for (int j = 0; j < col; j++)
+	//					{
+	//						mass[i][j] = mat1.mass[i][j] + mat2.mass[i][j];
+	//					}
+	//				}
+	//			}
+	//			break;
+	//		}
+	//
+	//		case '-':
+	//		{
+	//			if (mat1.str != mat2.str && mat1.col != mat2.col)
+	//			{
+	//				cout << "\nТипы матриц не совпадают \n";
+	//			}
+	//			else
+	//			{
+	//				this->str = mat1.str;
+	//				this->col = mat1.col;
+	//				this->mass = new int* [str];
+	//				for (int i = 0; i < str; i++) {
+	//					mass[i] = new int[col] {};
+	//					for (int j = 0; j < col; j++)
+	//					{
+	//						mass[i][j] = mat1.mass[i][j] - mat2.mass[i][j];
+	//					}
+	//				}
+	//			}
+	//			break;
+	//		}
+	//
+	//		case '*':
+	//		{
+	//			if (mat1.str != mat2.col && mat1.col != mat2.str)
+	//			{
+	//				cout << "\nТипы матриц не совпадают \n";
+	//			}
+	//			else
+	//			{
+	//				this->str = mat1.str;
+	//				this->col = mat2.col;
+	//				this->mass = new int* [str];
+	//				for (int i = 0; i < str; i++) {
+	//					mass[i] = new int[col] {};
+	//				}
+	//				for (int i = 0; i < str; i++)
+	//				{
+	//					for (int j = 0; j < mat2.str; j++)
+	//					{
+	//						for (int k = 0; k < col; k++)
+	//						{
+	//							this->mass[i][j] += mat1.mass[i][k] * mat2.mass[k][j];
+	//						}
+	//					}
+	//				}
+	//				break;
+	//			}
+	//		}
+	//		default:
+	//		{
+	//			cout << "\nНеизвестный оператор \n";
+	//			break;
+	//		}
+	//		}
+	//}
+};
 
-	Matrix(Matrix& mat1, Matrix& mat2, char oper) {
-		cout << "\nЗапустился конcтруктор с оператором " << this << "\n";
-		if (typeid(mat1.mass).name() != typeid(mat2.mass).name())
-		{
-			cout << "\nТипы матриц не совпадают \n";
+Matrix operator+ (const Matrix& mat1, const Matrix& mat2)
+{
+	cout << "+Assigment: \t\t" << "\n"; // << &temp 
+	Matrix temp(mat1.getStr(), mat1.getCol());
+	// + по математическому правилу
+	if (mat1.getStr() != mat2.getStr() && mat1.getCol() != mat2.getCol())
+	{
+		cout << "\nТипы матриц не совпадают \n";
+	}
+	else
+	{
+		for (int i = 0; i < temp.getStr(); i++) {
+			for (int j = 0; j < temp.getCol(); j++)
+			{
+				temp.setMass(i, j, mat1.getMass(i, j) + mat2.getMass(i, j));
+			}
 		}
-		else
-			switch (oper) {
-			case '+':
+	}
+	return temp;
+}
+Matrix operator- (const Matrix& mat1, const Matrix& mat2)
+{
+	cout << "-Assigment: \t\t" << "\n";// << &temp 
+	Matrix temp(mat1.getStr(), mat1.getCol());
+	// + по математическому правилу
+	if (mat1.getStr() != mat2.getStr() && mat1.getCol() != mat2.getCol())
+	{
+		cout << "\nТипы матриц не совпадают \n";
+	}
+	else
+	{
+		for (int i = 0; i < temp.getStr(); i++) {
+			for (int j = 0; j < temp.getCol(); j++)
 			{
-				if (mat1.str != mat2.str && mat1.col != mat2.col)
-				{
-					cout << "\nТипы матриц не совпадают \n";
-				}
-				else
-				{
-					this->str = mat1.str;
-					this->col = mat1.col;
-					this->mass = new T * [str];
-					for (int i = 0; i < str; i++) {
-						mass[i] = new T[col]{};
-						for (int j = 0; j < col; j++)
-						{
-							mass[i][j] = mat1.mass[i][j] + mat2.mass[i][j];
-						}
-					}
-				}
-				break;
+				temp.setMass(i, j, mat1.getMass(i, j) - mat2.getMass(i, j));
 			}
-
-			case '-':
+		}
+	}
+	return temp;
+}
+Matrix operator* (const Matrix& mat1, const Matrix& mat2)
+{
+	cout << "*Assigment: \t\t" << "\n";// << &temp 
+	Matrix temp(mat1.getStr(), mat2.getCol());
+	// + по математическому правилу
+	if (mat1.getStr() != mat2.getCol() && mat1.getCol() != mat2.getStr())
+	{
+		cout << "\nТипы матриц не совпадают \n";
+	}
+	else
+	{
+		for (int i = 0; i < temp.getStr(); i++)
+		{
+			for (int j = 0; j < mat2.getStr(); j++)
 			{
-				if (mat1.str != mat2.str && mat1.col != mat2.col)
+				for (int k = 0; k < temp.getCol(); k++)
 				{
-					cout << "\nТипы матриц не совпадают \n";
+					temp.setMass(i, j, temp.getMass(i, j) + mat1.getMass(i, k) * mat2.getMass(k, j));
 				}
-				else
-				{
-					this->str = mat1.str;
-					this->col = mat1.col;
-					this->mass = new T * [str];
-					for (int i = 0; i < str; i++) {
-						mass[i] = new T[col]{};
-						for (int j = 0; j < col; j++)
-						{
-							mass[i][j] = mat1.mass[i][j] - mat2.mass[i][j];
-						}
-					}
-				}
-				break;
 			}
-
-			case '*':
-			{
-				if (mat1.str != mat2.col && mat1.col != mat2.str)
-				{
-					cout << "\nТипы матриц не совпадают \n";
-				}
-				else
-				{
-					this->str = mat1.str;
-					this->col = mat2.col;
-					this->mass = new T * [str];
-					for (int i = 0; i < str; i++) {
-						mass[i] = new T[col]{};
-					}
-					for (int i = 0; i < str; i++)
-					{
-						for (int j = 0; j < mat2.str; j++)
-						{
-							for (int k = 0; k < col; k++)
-							{
-								this->mass[i][j] += mat1.mass[i][k] * mat2.mass[k][j];
-							}
-						}
-					}
-					break;
-				}
-			};
-			default:
-			{
-				cout << "\nНеизвестный оператор \n";
-				break;
-			};
-			};
-	};
+		}
+	}
+	return temp;
 }
 
-; int main()
+
+int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	Matrix <int>a(4, 4);// создаем новый объект
+	Matrix a(4, 4);// создаем новый объект
 	a.printMatrix();// выводим объект
 	a.inicialis();// заполняем случайными числами
 	a.printMatrix();// выводим объект
+	Matrix a1 = a;// создаем новый объект
+	a1.printMatrix();// выводим объект
 	cout << "\nМаксимум " << a.max() << " \n";
 	cout << "\nМинимум " << a.min() << " \n";
-	Matrix <int>b(4, 4);// создаем новый объект
+	Matrix b(4, 4);// создаем новый объект
 	b.inicialis();// заполняем случайными числами
 	b.printMatrix();// выводим объект
-	Matrix <int>c1(a, b, '+'); // суммирование
+	Matrix c1 = a + b; // суммирование
+	//Matrix c1(a, b, '+'); // суммирование
 	c1.printMatrix();
-	Matrix <int>c2(a, b, '-'); // разность
+	Matrix c2 = a - b; // разность
+	//Matrix c2(a, b, '-'); // разность
 	c2.printMatrix();
-	Matrix <int>c3(a, b, '*'); // умножаем
+	Matrix c3 = a * b; // умножаем
+	//Matrix c3(a, b, '*'); // умножаем
 	c3.printMatrix();
 
 
